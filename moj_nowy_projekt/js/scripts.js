961375de8a85e75c10a8d12ea63eb128da4f7244
Michał Gacek <gacek.michael@outlook.com>
2016-12-03 17:10:56 +0100

    Fix to format code

--- moj_nowy_projekt/js/scripts.js
+++ moj_nowy_projekt/js/scripts.js
@@ -1,16 +1,12 @@
-var a = prompt('wpisz pierwszą liczbę')
-var b = prompt('wpisz drugą liczbę')
-var wynik = a*a + a*b + b*b
-if (wynik>0)
-  {
-  console.log('wynik jest dodatni i wynosi=' + wynik)
-  }
-else if (wynik<0)
-  {
-  console.log('wynik jest ujemny i wynosi=' + wynik)
-  }
-else if (wynik==0)
-  {
-   console.log('wynik jest równy 0')
-  }
+var a = prompt('wpisz pierwszą liczbę'),
+    b = prompt('wpisz drugą liczbę'),
+    wynik = a*a + a*b + b*b;
+
+if (wynik > 0) {
+  console.log('wynik jest dodatni i wynosi=' + wynik);
+} else if (wynik < 0) {
+  console.log('wynik jest ujemny i wynosi=' + wynik);
+} else {
+   console.log('wynik jest równy 0');
+}
   
