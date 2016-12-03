9112713727f579706f0d239d4a5fb0eb3e1d904a
ibserd <ibserd@gmail.com>
2016-12-03 14:47:54 +0100

    trzeci

--- moj_nowy_projekt/js/scripts.js
+++ moj_nowy_projekt/js/scripts.js
@@ -1,4 +1,16 @@
-var a = prompt('wpisz wartość długości podstawy')
-var h = prompt('wpisz wartość wysokości trójkąta')
-var triangleArea = a*h/2
-alert('Pole trójkąta wynosi ' + triangleArea)
+var a = prompt('wpisz pierwszą liczbę')
+var b = prompt('wpisz drugą liczbę')
+var wynik = a*a + a*b + b*b
+if (wynik>0)
+  {
+  console.log('wynik jest dodatni i wynosi=' + wynik)
+  }
+else if (wynik<0)
+  {
+  console.log('wynik jest ujemny i wynosi=' + wynik)
+  }
+else if (wynik==0)
+  {
+   console.log('wynik jest równy 0')
+  }
+  
