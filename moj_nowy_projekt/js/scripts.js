f9f6803a149473c4e5b5cd7e57b8f699d3d66733
Michał Gacek <gacek.michael@outlook.com>
2016-12-03 17:15:46 +0100

    Format code

--- moj_nowy_projekt/js/scripts.js
+++ moj_nowy_projekt/js/scripts.js
@@ -1,15 +1,11 @@
 var  a = prompt('wpisz pierwszą liczbę'),
      b = prompt('wpisz drugą liczbę'),
      wynik = a*a + 2*a*b + b*b ;
-if (wynik>0)
-  {
+
+if (wynik > 0) {
   console.log('wynik jest dodatni i wynosi=' + wynik);
-  }
-else if (wynik<0)
-  {
+} else if (wynik < 0) {
   console.log('wynik jest ujemny i wynosi=' + wynik);
-  }
-else
-  {
+} else {
    console.log('wynik jest równy 0');
-  }
+}
