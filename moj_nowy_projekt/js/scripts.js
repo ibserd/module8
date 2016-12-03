dc432e7dbed41fda1178e8d225b80fb7c03e9ea7
ibserd <ibserd@gmail.com>
2016-12-03 17:10:21 +0100

    Update scripts.js

--- moj_nowy_projekt/js/scripts.js
+++ moj_nowy_projekt/js/scripts.js
@@ -3,13 +3,13 @@ var  a = prompt('wpisz pierwszą liczbę'),
      wynik = a*a + 2*a*b + b*b ;
 if (wynik>0)
   {
-  console.log('wynik jest dodatni i wynosi=' + wynik)
+  console.log('wynik jest dodatni i wynosi=' + wynik);
   }
 else if (wynik<0)
   {
-  console.log('wynik jest ujemny i wynosi=' + wynik)
+  console.log('wynik jest ujemny i wynosi=' + wynik);
   }
 else
   {
-   console.log('wynik jest równy 0')
+   console.log('wynik jest równy 0');
   }
