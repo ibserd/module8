f343e78c6219ba7c3b1ab0d4903aeb3dfc4ffad3
ibserd <ibserd@gmail.com>
2016-12-03 17:08:18 +0100

    Update scripts.js

--- moj_nowy_projekt/js/scripts.js
+++ moj_nowy_projekt/js/scripts.js
@@ -1,6 +1,6 @@
 var a = prompt('wpisz pierwszą liczbę')
 var b = prompt('wpisz drugą liczbę')
-var wynik = a*a + a*b + b*b
+var wynik = a*a + 2*a*b + b*b
 if (wynik>0)
   {
   console.log('wynik jest dodatni i wynosi=' + wynik)
@@ -9,7 +9,7 @@ else if (wynik<0)
   {
   console.log('wynik jest ujemny i wynosi=' + wynik)
   }
-else if (wynik==0)
+else
   {
    console.log('wynik jest równy 0')
   }
